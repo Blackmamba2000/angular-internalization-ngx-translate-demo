0a5cbb820f1e90377e77d9d06c72e0faa0f2f5f3
Karl King <knking85@gmail.com>
2019-08-20 07:38:49 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,14 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
 
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'es']);
+    translate.addLangs(['en', 'es', 'fr', 'jp']);
     translate.setDefaultLang('en');
+
+    // Gets the default language from the Web Browser your using
+    const browserLang = translate.getBrowserCultureLang();
+
+    // Determines if the browser has either English or French languages.
+    translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
   }
 
 }
